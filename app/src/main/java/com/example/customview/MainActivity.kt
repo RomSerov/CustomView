eb69e88b8f7034a73462ae21f57ff6f2a084dbae
romanserov <romanserov>
2023-10-13 22:39:21 +0300

    WaterBottle

--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -3,6 +3,25 @@ package com.example.customview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.customview.ui.theme.CustomViewTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +29,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CustomViewTheme {
-                
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.onBackground
+                ) {
+                    var usedWaterAmount by remember {
+                        mutableStateOf(100f)
+                    }
+                    val totalWaterAmount = remember {
+                        2300f
+                    }
+
+                    val buttonState = remember {
+                        mutableStateOf(true)
+                    }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        WaterBottle(
+                            totalWaterAmount = totalWaterAmount,
+                            unit = "ml",
+                            usedWaterAmount = usedWaterAmount
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(
+                            text = "Total Amount is : $totalWaterAmount",
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(
+                            enabled = buttonState.value,
+                            onClick = {
+                                      if (usedWaterAmount < totalWaterAmount) usedWaterAmount += 200 else buttonState.value = false
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xff279EFF),
+                                disabledContainerColor = Color.LightGray
+                            )
+                        ) {
+                            Text(text = "Drink")
+                        }
+                    }
+                }
             }
         }
     }
